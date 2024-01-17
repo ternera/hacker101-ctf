c358d07284611188f88e86fb0ce4a5022a5936bf
ternera <contact@ternera.org>
2024-01-17 10:28:55 -0600

    Update replace_base64.py

--- replace_base64.py
+++ replace_base64.py
@@ -1,3 +1,4 @@
+# for the encrypted pastebin challenge
 string = "https://bcca65a9a395227f1a54fee96dec6c37.ctf.hacker101.com/?post=duXbzT!-FHd!YbxlzkUYasxaXvZgSYYmc8rXWoeWC!NVZtm9XGymMrX-UgAl4M-nQ0c5Ke67DtfCqdvpM13w83hQ0AT7GQeh0T2wtMS0Eiv1HkiHuRBTNwqtCdSWum94piT9BZpm4yzI!3PZzYYwhjcqYX6oIBUx3vV7EU9LHod4EAjE4!6HOtWN87FhE9oIs-MsO98Wa6kmN5FQyFLLcA~~"
  
 # replace the following characters and then print the new string
